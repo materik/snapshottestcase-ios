b566a174ce7b4ae0c8fd674a245d3b8a79d42363
Mattias Farnemyhr <mattias@materikab.com>
2022-12-13 20:55:28 +0100

    Update SnapshotConfig api

--- Sources/SnapshotTestCase/SnapshotConfig.swift
+++ Sources/SnapshotTestCase/SnapshotConfig.swift
@@ -1,26 +1,22 @@
 import Foundation
 
 public struct SnapshotConfig {
-    public struct Config: Identifiable {
-        public let device: Device
-        public let interfaceStyle: InterfaceStyle
+    struct Config: Identifiable {
+        let device: Device
+        let interfaceStyle: InterfaceStyle
         
-        public init(device: Device, interfaceStyle: InterfaceStyle) {
+        init(device: Device, interfaceStyle: InterfaceStyle) {
             self.device = device
             self.interfaceStyle = interfaceStyle
         }
         
-        public init() {
-            self.device = .default
-            self.interfaceStyle = .default
-        }
-        
-        public var id: String {
+        var id: String {
             [device.id, interfaceStyle.id].joined(separator: "_")
         }
     }
     
-    public let configs: [Config]
+    let configs: [Config]
+    public var count: Int { configs.count }
     
     public init() {
         configs = []
@@ -32,17 +28,22 @@ public struct SnapshotConfig {
 }
 
 public extension SnapshotConfig {
-    func add(_ config: Config) -> SnapshotConfig {
-        SnapshotConfig(configs + [config])
+    func add(device: Device) -> SnapshotConfig {
+        add(Config(device: device, interfaceStyle: .light))
+            .add(Config(device: device, interfaceStyle: .dark))
     }
     
-    var count: Int {
-        configs.count
+    func add(device: Device, interfaceStyle: InterfaceStyle) -> SnapshotConfig {
+        add(Config(device: device, interfaceStyle: interfaceStyle))
+    }
+    
+    private func add(_ config: Config) -> SnapshotConfig {
+        SnapshotConfig(configs + [config])
     }
 }
 
 public extension SnapshotConfig {
-    static var `default` = SnapshotConfig([Config()])
+    static var `default` = SnapshotConfig().add(device: .default)
 }
 
 extension SnapshotConfig.Config {
