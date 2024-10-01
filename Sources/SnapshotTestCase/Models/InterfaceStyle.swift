c80bfa783dcc6c71e05cb5857dd781f0da558246
Mattias Farnemyhr <mattias@materikab.com>
2024-10-01 12:01:44 +0200

    Add a new interface style for widget rendering mode

--- Sources/SnapshotTestCase/Models/InterfaceStyle.swift
+++ Sources/SnapshotTestCase/Models/InterfaceStyle.swift
@@ -1,10 +1,27 @@
 import UIKit
+import WidgetKit
+import SwiftUI
 
-public enum InterfaceStyle: String, Identifiable, Sendable {
+public enum InterfaceStyle: Identifiable, Sendable {
     case light
     case dark
+    case widgetRenderingMode(WidgetRenderingMode)
 
-    public var id: String { rawValue }
+    public var id: String {
+        switch self {
+        case .light: "light"
+        case .dark: "dark"
+        case .widgetRenderingMode(let mode): "widget_\(mode)"
+        }
+    }
+    
+    init?(id: String) {
+        switch id {
+        case InterfaceStyle.light.id: self = .light
+        case InterfaceStyle.dark.id: self = .dark
+        default: return nil
+        }
+    }
 }
 
 public extension InterfaceStyle {
@@ -14,8 +31,47 @@ public extension InterfaceStyle {
 extension InterfaceStyle {
     var overrideUserInterfaceStyle: UIUserInterfaceStyle {
         switch self {
-        case .dark: return .dark
-        case .light: return .light
+        case .dark:  .dark
+        case .light:  .light
+        case .widgetRenderingMode:  .dark
         }
+    }
+}
+
+extension InterfaceStyle: Equatable {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.id == rhs.id
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func interfaceStyle(_ interfaceStyle: InterfaceStyle) -> some View {
+        switch interfaceStyle {
+        case .light: environment(\.colorScheme, .light)
+        case .dark: environment(\.colorScheme, .dark)
+        case .widgetRenderingMode(let mode): environment(\.widgetRenderingMode, mode)
+        }
+    }
+}
+
+extension UIViewController {
+    func interfaceStyle(_ interfaceStyle: InterfaceStyle) -> UIViewController {
+        var viewController = self
+        if let hostingController = self as? AnyUIHostingController {
+            viewController = hostingController.interfaceStyle(interfaceStyle)
+        }
+        viewController.overrideUserInterfaceStyle = interfaceStyle.overrideUserInterfaceStyle
+        return viewController
+    }
+}
+
+private protocol AnyUIHostingController: UIViewController {
+    func rootViewInterfaceStyle(_ interfaceStyle: InterfaceStyle) -> UIHostingController<AnyView>
+}
+
+ extension UIHostingController: AnyUIHostingController {
+     fileprivate func rootViewInterfaceStyle(_ interfaceStyle: InterfaceStyle) -> UIHostingController<AnyView> {
+         .init(rootView: AnyView(rootView.interfaceStyle(interfaceStyle)))
     }
 }
